9afb78be933bc18ae1b79c6c12c71c512e39cbc2
Joe Jordan <ioexiordan@gmail.com>
2019-11-26 11:57:26 -0500

    Input, Output y viewchild

---
+++ src/app/pages/graficodona/graficodona.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-graficodona',
+  templateUrl: './graficodona.component.html',
+  styles: []
+})
+export class GraficodonaComponent implements OnInit {
+
+  // Doughnut
+  @Input('chartLabels') doughnutChartLabels: Label[] = [];
+  @Input('chartData') doughnutChartData: MultiDataSet = [];
+  @Input('chartType') doughnutChartType: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { PAGES_ROUTES } from './pages.routes';
 
 import { SharedModule } from '../shared/shared.module';
+
+import { FormsModule } from '@angular/forms';
+
+// ng2- charts
+import { ChartsModule } from 'ng2-charts';
+
 import { PagesComponent } from './pages.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficodonaComponent } from './graficodona/graficodona.component';
+
+// Temporal
 
 
 @NgModule({
@@ -14,16 +24,21 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficodonaComponent
     ],
     exports: [
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        GraficodonaComponent
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 
